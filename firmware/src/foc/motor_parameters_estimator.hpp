f46bdfcd3cf7fb973ae62586ae9420d19ff726b8
Pavel Kirienko <pavel.kirienko@gmail.com>
2016-09-16 13:37:06 +0300

    Fixed Phi measurement

--- firmware/src/foc/motor_parameters_estimator.hpp
+++ firmware/src/foc/motor_parameters_estimator.hpp
@@ -68,7 +68,7 @@ class MotorParametersEstimator
     static constexpr Scalar RsMeasurementDuration       = 10.0F;
     static constexpr Scalar LsMeasurementDuration       = 10.0F;
 
-    static constexpr Scalar PhiMeasurementFullRangeSweepDuration   = 10.0F;
+    static constexpr Scalar PhiMeasurementFullRangeSweepDuration = 15.0F;
 
     static constexpr unsigned IdqMovingAverageLength = 5;
 
@@ -384,8 +384,9 @@ public:
         case State::PhiMeasurement:
         {
             constexpr int IdxVoltage = 0;
-            constexpr int IdxAngVel = 1;
-            constexpr int IdxAngPos = 2;
+            constexpr int IdxAngVel  = 1;
+            constexpr int IdxAngPos  = 2;
+            constexpr int IdxPhi     = 3;
 
             // This is the maximum voltage we start from.
             Const initial_voltage = estimation_current_ * (result_.r_ab / 2.0F) * (3.0F / 2.0F);
@@ -403,6 +404,7 @@ public:
                 state_variables_[IdxVoltage] = initial_voltage;
                 state_variables_[IdxAngVel] = 0;
                 state_variables_[IdxAngPos] = 0;
+                state_variables_[IdxPhi] = 0;
 
                 switchState(State::PhiMeasurementAcceleration, true);
             }
@@ -418,22 +420,29 @@ public:
             }
             else if (state_ == State::PhiMeasurement)
             {
-                state_variables_[IdxVoltage] -= (initial_voltage / PhiMeasurementFullRangeSweepDuration) * pwm_period_;
-
                 Const Uq = state_variables_[IdxVoltage];
-                Const Iq = Idq[1];
+                Const Iq = currents_filter_.getValue()[1];
                 Const w = state_variables_[IdxAngVel];
                 Const Rs = result_.r_ab / 2.0F;
 
-                Const Phi = (Uq - Iq * Rs) / w;
+                Const new_phi = (Uq - Iq * Rs) / w;
 
-                // The true Phi is the maximum we'll encounter over the whole test
-                result_.field_flux = std::max(result_.field_flux, Phi);
+                state_variables_[IdxPhi] += 0.01F * (new_phi - state_variables_[IdxPhi]);
+
+                // The true Phi is the maximum we'll encounter over the whole test. Right, Dmitry?
+                result_.field_flux = std::max(result_.field_flux, state_variables_[IdxPhi]);
+
+                // Decreasing voltage
+                state_variables_[IdxVoltage] -= (initial_voltage / PhiMeasurementFullRangeSweepDuration) * pwm_period_;
 
                 if (state_variables_[IdxVoltage] <= 0.1F)
                 {
                     switchState(State::Finalization);
                 }
+            }
+            else
+            {
+                assert(false);
             }
 
             state_variables_[IdxAngPos] =
