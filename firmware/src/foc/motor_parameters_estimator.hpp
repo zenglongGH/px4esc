c6a027768ae978432a8cf766c16617e3af3dca48
Pavel Kirienko <pavel.kirienko@gmail.com>
2016-09-24 17:28:53 +0300

    RsHF/LsHF

--- firmware/src/foc/motor_parameters_estimator.hpp
+++ firmware/src/foc/motor_parameters_estimator.hpp
@@ -396,8 +396,17 @@ public:
 
                 const double Ud_square = square_double(averagers_[0].getAverage());
                 const double Uq_square = square_double(averagers_[1].getAverage());
+                const double Iq_square = square_double(estimation_current_);
+
+                Const RsHF = Scalar(std::sqrt(Uq_square / Iq_square));
+
+                Const LsHF = Scalar(std::sqrt(Ud_square / (Iq_square * double(w) * double(w))));
 
                 Const RoverL = Scalar(std::sqrt((Uq_square * double(w) * double(w)) / Ud_square));
+
+                IRQDebugOutputBuffer::setVariableFromIRQ<0>(RsHF);
+                IRQDebugOutputBuffer::setVariableFromIRQ<1>(LsHF);
+                IRQDebugOutputBuffer::setVariableFromIRQ<2>(RoverL);
 
                 result_.l_ab = result_.r_ab / RoverL;
 
