b65ea72f667edaf5d54aa8a338e04e8a972a5629
Pavel Kirienko <pavel.kirienko@gmail.com>
2016-10-13 17:26:30 +0300

    Fixed coverity scan build script

--- firmware/coverity_scan_build.sh
+++ firmware/coverity_scan_build.sh
@@ -4,9 +4,6 @@
 # and prepares the archive for uploading to the cloud static analyzer.
 #
 
-OUTPUT_DIR=build/cov-int
-OUTPUT_ARCHIVE=coverity.tgz
-
 function die() { echo "$@" 1>&2; exit 1; }
 
 which cov-configure && which cov-build || die "Coverity Build Tool is not in PATH"
@@ -14,9 +11,8 @@ which cov-configure && which cov-build || die "Coverity Build Tool is not in PAT
 cov-configure --comptype gcc --compiler arm-none-eabi-gcc --template
 
 make clean                              || die "Clean failed"
-rm -rf $OUTPUT_DIR $OUTPUT_ARCHIVE &> /dev/null
-mkdir -p $OUTPUT_DIR
-cov-build --dir $OUTPUT_DIR make -j8    || die "Build failed"
-tar czvf $OUTPUT_ARCHIVE $OUTPUT_DIR
+cov-build --dir build/cov-int make -j8  || die "Build failed"
+cd build
+tar czvf coverity.tgz cov-int
 
-echo "Done. Please submit the archive '$OUTPUT_ARCHIVE' to Coverity Scan now."
+echo "Done. Please submit the archive to Coverity Scan now."
