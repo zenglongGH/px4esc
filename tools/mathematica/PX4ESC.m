9a65b05563f3cd2335f4585dcb56374b72bef81e
Pavel Kirienko <pavel.kirienko@gmail.com>
2016-11-04 13:21:37 +0300

    Plot parsing module for Mathematica

---
+++ tools/mathematica/PX4ESC.m
@@ -0,0 +1,23 @@
+(*
+ * Functions for parsing firmware generated data dumps for plotting.
+ * Example usage:
+ *
+ *  Needs["PX4ESC`", "tools/mathematica/PX4ESC.m"]
+ *  plots = PX4ESC`splitXY[PX4ESC`parseFile["file.txt"]];
+ *)
+
+BeginPackage["PX4ESC`"]
+
+parseFile[file_] := Module[{
+   strings = Select[Import[file, "Lines"], StringLength[#1] > 0 && Characters[#1][[1]] == "$" &],
+   lines,
+   varsPerLine
+  },
+  lines = Map[Map[ToExpression, StringSplit[StringDrop[#, 1], ","]] &, strings];
+  varsPerLine = Median[Map[Length, lines]];
+  Select[lines, Length[#] == varsPerLine &]
+];
+
+splitXY[lines_] := Map[{lines\[Transpose][[1]], #}\[Transpose] &, lines\[Transpose][[2 ;;]]];
+
+EndPackage[]
